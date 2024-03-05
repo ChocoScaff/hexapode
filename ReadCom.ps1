6d3ddd143854d6a81aca9c5ef4f36c56e7b7e7e0
ChocoScaff <anjul2@hotmail.fr>
2024-03-05 16:54:28 +0100

    Add ports and ReadCom

---
+++ ReadCom.ps1
@@ -0,0 +1,12 @@
+echo readport
+$COM = [System.IO.Ports.SerialPort]::getportnames()
+
+function read-com {
+    $port= new-Object System.IO.Ports.SerialPort COM3 ,9600,None,8,one
+    $port.Open()
+    do {
+        $line = $port.ReadLine()
+        Write-Host $line # Do stuff here
+    }
+    while ($port.IsOpen)
+}
