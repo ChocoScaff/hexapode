603a30bca68e2156e1207578279f1feb36cf6438
fortune <fortune@fortune.place>
2024-05-29 15:52:17 +0200

    psuh sur main

--- f103-master/appli/main.c
+++ f103-master/appli/main.c
@@ -1,11 +1,3 @@
-/**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Nirgal
-  * @date    03-July-2019
-  * @brief   Default main function.
-  ******************************************************************************
-*/
 #include "stm32f1xx_hal.h"
 #include "stm32f1_uart.h"
 #include "stm32f1_sys.h"
@@ -20,7 +12,7 @@
  */
 void writeLED(bool_e b)
 {
-	HAL_GPIO_WritePin(LED_GREEN_GPIO, LED_GREEN_PIN, b);
+    HAL_GPIO_WritePin(LED_GREEN_GPIO, LED_GREEN_PIN, b);
 }
 
 /**
@@ -29,90 +21,51 @@ void writeLED(bool_e b)
  */
 bool_e readButton(void)
 {
-	return !HAL_GPIO_ReadPin(BLUE_BUTTON_GPIO, BLUE_BUTTON_PIN);
+    return !HAL_GPIO_ReadPin(BLUE_BUTTON_GPIO, BLUE_BUTTON_PIN);
 }
 
 static volatile uint32_t t = 0;
 void process_ms(void)
 {
-	if(t)
-		t--;
+    if(t)
+        t--;
 }
 
+#define NUM_SERVOS 12
 
 int main(void)
 {
-	//Initialisation de la couche logicielle HAL (Hardware Abstraction Layer)
-	//Cette ligne doit rester la premi�re �tape de la fonction main().
-	HAL_Init();
+    HAL_Init();
+    UART_init(UART2_ID, 115200);
+    SYS_set_std_usart(UART2_ID, UART2_ID, UART2_ID);
+    BSP_GPIO_PinCfg(LED_GREEN_GPIO, LED_GREEN_PIN, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL, GPIO_SPEED_FREQ_HIGH);
+    BSP_GPIO_PinCfg(BLUE_BUTTON_GPIO, BLUE_BUTTON_PIN, GPIO_MODE_INPUT, GPIO_PULLUP, GPIO_SPEED_FREQ_HIGH);
+    Systick_add_callback_function(&process_ms);
 
-	//Initialisation de l'UART2 � la vitesse de 115200 bauds/secondes (92kbits/s) PA2 : Tx  | PA3 : Rx.
-		//Attention, les pins PA2 et PA3 ne sont pas reli�es jusqu'au connecteur de la Nucleo.
-		//Ces broches sont redirig�es vers la sonde de d�bogage, la liaison UART �tant ensuite encapsul�e sur l'USB vers le PC de d�veloppement.
-	UART_init(UART2_ID,115200);
+    Servo_t servos[NUM_SERVOS];
 
-	//"Indique que les printf sortent vers le p�riph�rique UART2."
-	SYS_set_std_usart(UART2_ID, UART2_ID, UART2_ID);
+    // Initialize all servos
+    for (int i = 0; i < NUM_SERVOS; i++) {
+        uint16_t timer = TIMER1_ID + (i / 4); // Distribute across multiple timers if necessary
+        uint16_t channel = TIM_CHANNEL_1 + (i % 4) * 4;
+        SERVO_init(&servos[i], timer, channel);
+    }
 
-	//Initialisation du port de la led Verte (carte Nucleo)
-	BSP_GPIO_PinCfg(LED_GREEN_GPIO, LED_GREEN_PIN, GPIO_MODE_OUTPUT_PP,GPIO_NOPULL,GPIO_SPEED_FREQ_HIGH);
-
-	//Initialisation du port du bouton bleu (carte Nucleo)
-	BSP_GPIO_PinCfg(BLUE_BUTTON_GPIO, BLUE_BUTTON_PIN, GPIO_MODE_INPUT,GPIO_PULLUP,GPIO_SPEED_FREQ_HIGH);
-
-	//On ajoute la fonction process_ms � la liste des fonctions appel�es automatiquement chaque ms par la routine d'interruption du p�riph�rique SYSTICK
-	Systick_add_callback_function(&process_ms);
-
-	Servo_t servo1;
-    Servo_t servo2;
-    Servo_t servo3;
-    Servo_t servo4;
-    Servo_t servo5;
-    Servo_t servo6;
-    Servo_t servo7;
-    Servo_t servo8;
-    Servo_t servo9;
-    Servo_t servo10;
-    Servo_t servo11;
-    Servo_t servo12;
-
-	SERVO_init(&servo1, TIMER1_ID, TIM_CHANNEL_1);
-    SERVO_init(&servo2, TIMER1_ID, TIM_CHANNEL_2);
-    SERVO_init(&servo3, TIMER1_ID, TIM_CHANNEL_3);
-    SERVO_init(&servo4, TIMER1_ID, TIM_CHANNEL_4);
-    SERVO_init(&servo5, TIMER2_ID, TIM_CHANNEL_1);
-    SERVO_init(&servo6, TIMER2_ID, TIM_CHANNEL_2);
-    SERVO_init(&servo7, TIMER2_ID, TIM_CHANNEL_3);
-    SERVO_init(&servo8, TIMER2_ID, TIM_CHANNEL_4);
-    SERVO_init(&servo9, TIMER3_ID, TIM_CHANNEL_1);
-    SERVO_init(&servo10, TIMER3_ID, TIM_CHANNEL_2);
-    SERVO_init(&servo11, TIMER3_ID, TIM_CHANNEL_3);
-    SERVO_init(&servo12, TIMER3_ID, TIM_CHANNEL_4);
-
-    while(1)
+    while (1)
     {
         if(!t)
         {
             t = 200; // Refresh rate
             HAL_GPIO_TogglePin(LED_GREEN_GPIO, LED_GREEN_PIN);
 
-            // Example of moving the servo: Modify the duty cycle to move the servo
-            // Here, just toggling between two positions as an example
-            SERVO_set_position(&servo1, 50);
-            SERVO_set_position(&servo2, 50);
-            SERVO_set_position(&servo3, 50);
-            SERVO_set_position(&servo4, 50);
-            SERVO_set_position(&servo5, 50);
-            SERVO_set_position(&servo6, 50);
-            SERVO_set_position(&servo7, 50);
-            SERVO_set_position(&servo8, 50);
-            SERVO_set_position(&servo9, 50);
-            SERVO_set_position(&servo10, 50);
-            SERVO_set_position(&servo11, 50);
-            SERVO_set_position(&servo12, 50);
-            )
+            // Example of moving the servos: Modify the duty cycle to move the servos
+            for (int i = 0; i < NUM_SERVOS; i++) {
+                SERVO_set_position(&servos[i], 50);
+            }
         }
-        SERVO_set_position(&servo1, 0);
 
+        for (int i = 0; i < NUM_SERVOS; i++) {
+            SERVO_set_position(&servos[i], 0);
+        }
     }
 }
--- f103-master/appli/servo.c
+++ f103-master/appli/servo.c
@@ -1,11 +1,3 @@
-/**
- * @file servo.c
- *
- *  Created on: 29 févr. 2024
- *      Author: brouzean
- */
-
-
 #include "config.h"
 #include "stm32f1_timer.h"
 #include "stm32f1_gpio.h"
@@ -23,77 +15,63 @@
  */
 void SERVO_process_test(Servo_t *servo)
 {
-
-	static bool_e previous_button = FALSE;
-	bool_e current_button;
-	//lecture du bouton bleu
-	current_button = !HAL_GPIO_ReadPin(BLUE_BUTTON_GPIO, BLUE_BUTTON_PIN);
-	if(current_button && !previous_button) //si appui bouton
-	{
-		servo->current_position = (servo->current_position > 99)?0:(servo->current_position+5); //de 0 à 100%, par pas de 5%
-		SERVO_set_position(servo, servo->current_position);
-	}
-	previous_button = current_button; //sauvegarde pour le prochain passage
-	HAL_Delay(10); //anti-rebond "de fortune" en cadencant la lecture du bouton
+    static bool_e previous_button = FALSE;
+    bool_e current_button;
+    // Read the blue button
+    current_button = !HAL_GPIO_ReadPin(BLUE_BUTTON_GPIO, BLUE_BUTTON_PIN);
+    if (current_button && !previous_button) // If button is pressed
+    {
+        servo->current_position = (servo->current_position > 99) ? 0 : (servo->current_position + 5); // From 0 to 100%, step by 5%
+        SERVO_set_position(servo, servo->current_position);
+    }
+    previous_button = current_button; // Save for the next check
+    HAL_Delay(10); // Debouncing by timing the button reading
 }
 
-
 /**
- *
  * Init Servo_t
- *
  * @param servo
  * @param timer_id_e
- * @param TIM_Chanel
- *
+ * @param TIM_Channel
  */
-void SERVO_init(Servo_t *servo, timer_id_e timerid, uint16_t TIM_CHANNEL){
+void SERVO_init(Servo_t *servo, timer_id_e timerid, uint16_t TIM_CHANNEL)
+{
+    servo->tim_channel = TIM_CHANNEL;
+    servo->timerid = timerid;
+    servo->current_position = 50;
 
-
-	servo->tim_channel = TIM_CHANNEL;
-	servo->timerid = timerid;
-	servo->current_position = 50;
-
-	//initialisation et lancement du timer1 à une période de 10 ms
-	TIMER_run_us(servo->timerid, PERIOD_TIMER*1000, FALSE); //10000us = 10ms
-	//activation du signal PWM sur le canal 1 du timer 1 (broche PA8)
-	TIMER_enable_PWM(servo->timerid, servo->tim_channel, 150, FALSE, FALSE);
-	//rapport cyclique reglé pour une position servo de 50%
-	SERVO_set_position(servo, 50);
+    // Initialize and start timer with a period of 10 ms
+    TIMER_run_us(servo->timerid, PERIOD_TIMER * 1000, FALSE); // 10000us = 10ms
+    // Enable PWM signal on timer channel
+    TIMER_enable_PWM(servo->timerid, servo->tim_channel, 150, FALSE, FALSE);
+    // Set duty cycle for servo position of 50%
+    SERVO_set_position(servo, 50);
 }
 
-
 /**
- *
- *	Set new position
- *
+ * Set new position
  * @param servo
- * @param position est exprimée de 0 à 100.
- *
+ * @param position is expressed from 0 to 100.
  */
-void SERVO_set_position(Servo_t *servo, uint16_t position){
-	servo->current_position = position;
-	if(servo->current_position > 100)
-		servo->current_position = 100; //écretage si l'utilisateur demande plus de 100%
-    uint16_t duty_c = servo->current_position+100;
-	TIMER_set_duty(servo->timerid, servo->tim_channel, duty_c );
-
+void SERVO_set_position(Servo_t *servo, uint16_t position)
+{
+    servo->current_position = position;
+    if (servo->current_position > 100)
+        servo->current_position = 100; // Clamp if user requests more than 100%
+    uint16_t duty_c = servo->current_position + 100;
+    TIMER_set_duty(servo->timerid, servo->tim_channel, duty_c);
 }
 
-
 /**
- *
  * Add position to current position
- *
  * @param servo
- * @param position est exprimée de 0 à 100.
- *
+ * @param position is expressed from 0 to 100.
  */
-void SERVO_increment_position(Servo_t *servo, uint16_t position){
-	servo->current_position += position;
-	if(servo->current_position > 100)
-		servo->current_position = 100; //écretage si l'utilisateur demande plus de 100%
-    uint16_t duty_c = servo->current_position+100;
-	TIMER_set_duty(servo->timerid, servo->tim_channel, duty_c );
-
+void SERVO_increment_position(Servo_t *servo, uint16_t position)
+{
+    servo->current_position += position;
+    if (servo->current_position > 100)
+        servo->current_position = 100; // Clamp if user requests more than 100%
+    uint16_t duty_c = servo->current_position + 100;
+    TIMER_set_duty(servo->timerid, servo->tim_channel, duty_c);
 }
